ff4886b01c8ec725720f818e95d18a3ba2af0baa
Darren Dormer <me@darren.io>
2016-12-11 13:16:31 +0200

    Solve the second part of day 6.

--- python/day_6.py
+++ python/day_6.py
@@ -1,12 +1,6 @@
 from collections import defaultdict, OrderedDict
 
 
-columns = defaultdict(lambda: defaultdict(int))
-
-
-def record_character(column, character):
-    columns[column][character] += 1
-
 
 def parse(data):
     """
@@ -17,22 +11,46 @@ def parse(data):
     return data.split('\n')[:-1]
 
 
-def frequent(column):
+def record_characters(data):
+    """
+    Record the frequency of characters in each column.
+    """
+    columns = defaultdict(lambda: defaultdict(int))
+    for line in data:
+        for column, char in enumerate(line):
+            columns[column][char] += 1
+    return columns
+
+
+def most_frequent(column):
     """
     Return the most frequently recorded character
     """
     return sorted(column.items(), key=lambda i: i[1], reverse=True)[0][0]
 
 
+def least_frequent(column):
+    """
+    Return the least frequently recorded character
+    """
+    return sorted(column.items(), key=lambda i: i[1], reverse=False)[0][0]
+
+
 def solve_a(data):
     """
     Solve the first part of the challenge
     """
-    for line in data:
-        for column, char in enumerate(line):
-            record_character(column, char)
+    columns = record_characters(data)
+    output = [most_frequent(column) for k, column in columns.items()]
+    return ''.join(output)
 
-    output = [frequent(column) for k, column in columns.items()]
+
+def solve_b(data):
+    """
+    Solve the second part of the challenge
+    """
+    columns = record_characters(data)
+    output = [least_frequent(column) for k, column in columns.items()]
     return ''.join(output)
 
 
@@ -40,5 +58,5 @@ if __name__ == '__main__':
     with open('day_6.txt') as f:
         _input = f.read()
     data = parse(_input)
-
     print solve_a(data)
+    print solve_b(data)
